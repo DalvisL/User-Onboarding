10d3b2f4a0e873e1050aca8a7686e50edc63f145
Dalvis <Dalvis.liz27@gmail.com>
2023-02-01 21:15:51 -0500

    added submit form handler, user slice of state, and axios post request

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -2,9 +2,10 @@ import { useState, useEffect } from 'react';
 import Form from './Components/Form';
 import schema from './validation/schema'
 import * as yup from 'yup';
+import axios from 'axios';
 
 function App() {
-
+  // Slices of State
   const [formData, setFormData] = useState({ // Form Data state
     first_name: '',
     last_name: '',
@@ -19,27 +20,61 @@ function App() {
     password: '',
     terms: '' 
   })
-  const [disabled, setDisabled] = useState(true);
+  const [disabled, setDisabled] = useState(true);// Disabled button state 
+  const [users, setUsers] = useState([]);
 
+  const setFormErrors = (name, value) => {
+    yup.reach(schema, name)
+      .validate(value)
+      .then(valid => setErrors({...errors, [name]: ''}))
+      .catch(err => setErrors({...errors, [name]: err.errors[0]}))
+  }
   const change = (event) => { // onChange event handler function
    const  {name, type, checked, value} = event.target;
    const valueToUse = type === 'checkbox' ? checked : value;
    setFormData({...formData, [name] : valueToUse});
+   setFormErrors(name, valueToUse);
+  }
+  const submit = (event) => {
+    event.preventDefault()
+    const newUser = {
+      first_name: formData.first_name.trim(),
+      last_name: formData.last_name.trim(),
+      email: formData.email,
+      password: formData.password,
+      terms: formData.terms
+    }
+
+    axios.post('https://reqres.in/api/users', newUser)
+      .then(res => {
+        const newArr = [...users];
+        newArr.push(res.data);
+        setUsers(newArr);
+        console.log(users);
+        setFormData({
+          first_name: '',
+          last_name: '',
+          email: '',
+          password: '',
+          terms: false 
+        })
+      })
+      .catch(err => console.error)
   }
 
-  const setFormErrors = (name, value) => {
-    yup.reach(schema, name)
-      .validate()
-  }
-
-  useEffect(() => {
+  useEffect(() => { // sets button disabled
     schema.isValid(formData).then(valid => setDisabled(!valid));
   }
   ,[formData])
 
   return (
     <div className="App">
-      <Form formData={formData} setFormData={setFormData} change={change} disabled={disabled}/>
+      <Form formData={formData} change={change} submit={submit} disabled={disabled} errors={errors}/>
+      {users.length > 0 && users.map((user, index) => {
+        return(
+          <pre className='users'>{JSON.stringify(user)}</pre>
+        )
+      })}
     </div>
   );
 }
--- user-onboarding/src/Components/Form.js
+++ user-onboarding/src/Components/Form.js
@@ -6,11 +6,15 @@ const StyledForm = styled.form`
 `
 
 export default function Form(props) {
-    const {formData, setFormData, change, disabled} = props;
+    const {formData, change, submit, disabled, errors} = props;
   return (
     <div>
-        <StyledForm>
+        <StyledForm onSubmit={submit}>
             <div>
+                <div className='errors'>
+                    {errors.first_name.length > 0 && <p>{errors.first_name}</p>}
+                    {errors.last_name.length > 0 && <p>{errors.last_name}</p>}
+                </div>
                 <label> First Name
                     <input onChange={change} type='text' value={formData.first_name} name='first_name' placeholder='First Name' ></input>
                 </label>  
@@ -18,16 +22,23 @@ export default function Form(props) {
                     <input onChange={change} type='text' value={formData.last_name} name='last_name' placeholder='Last Name'></input>
                 </label> 
             </div>
-            <label> Email
-                <input onChange={change} type='email' value={formData.email} name='email' placeholder='Email Address'></input>
-            </label>
-            <label> Password
-                <input onChange={change} type='text' value={formData.password} name='password' placeholder='password'></input>
-            </label>
+            <div>
+                <div className='errors'>
+                    {errors.email.length > 0 && <p>{errors.email}</p>}
+                    {errors.password.length > 0 && <p>{errors.password}</p>}
+                </div>
+                <label> Email
+                    <input onChange={change} type='email' value={formData.email} name='email' placeholder='Email Address'></input>
+                </label>
+                <label> Password
+                    <input onChange={change} type='password' value={formData.password} name='password' placeholder='Password'></input>
+                </label>  
+            </div>
+            {errors.terms.length > 0 && <p>{errors.terms}</p>}
             <label> Agree to the Terms and Conditions
                 <input onChange={change} type='checkbox' value={formData.terms} name='terms'></input>
             </label>
-                
+            <button type='submit' disabled={disabled}>Submit</button>
         </StyledForm>
     </div>
   )
